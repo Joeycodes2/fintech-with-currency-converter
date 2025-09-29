d18e702e07647b51dc92338a564dd6e04a628a84
Joeycodes <joeycodes2@gmail.com>
2025-09-29 01:20:10 +0100

    cover more country codes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const PORT = process.env.PORT || 5000;
 const app = express(); 
@@ -9,26 +10,29 @@ const app = express();
 const API_URL = 'https://v6.exchangerate-api.com/v6';
 const API_KEY = process.env.EXCHANGE_RATE_API_KEY;
 const apiLimiter = rateLimit({
-    windowMs: 20 * 60 * 1000, //unlimited requests per/30minutes
+    windowMs: 20 * 60 * 1000, //unlimited requests per/20minutes
     max: 100 //limit each ip to 100 requests per/window
 }) 
 
-
+// cors options
+const corsOptions = {
+    origin:['http://localhost:5173']
+}
 //Middlewares
 app.use(express.json()); //to parse incoming json data
 app.use(apiLimiter);
+app.use(cors(corsOptions));
+
 
 
 
 //conversion router 
 app.post("/api/convert", async(req, res) => {
     try {
-        //get user data
     const { from, to, amount } = req.body;
-    //construct the pi
+    //Api construct
     const url = `${API_URL}/${API_KEY }/pair/${from}/${to}/${amount}`;
     const response = await axios.get(url);
-    // console.log(response);
     if(response.data && response.data.result === "success"){
         res.json({
             base: from, 
@@ -45,5 +49,5 @@ app.post("/api/convert", async(req, res) => {
     }
 });
 
-//spin the server
+//spin the server.
 app.listen(PORT, console.log(`server is running on PORT ${PORT}`));
